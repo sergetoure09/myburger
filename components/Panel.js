4e4b65f1329c7987f922a0a8f52f104e90e953d8
sergetoure09 <sergetoure09@gmail.com>
2018-03-05 22:36:51 +0000

    add remove

--- components/Panel.js
+++ components/Panel.js
@@ -9,8 +9,10 @@ const Panel=props=>{
         <Aux classes="controller">
         <span className="controller__ingredient">Cheese</span>
         <div className="controller__control-btn">
-        <button className=" controller__btn controler__btn--add">Add</button>
-        <button className=" controller__btn controler__btn--remove">Remove</button>
+        {/* <button className=" controller__btn controler__btn--add">Add</button> */}
+        <i class="material-icons controller__btn controler__btn--add">add</i>
+        {/* <button className=" controller__btn controler__btn--remove">Remove</button> */}
+        <i class="material-icons controller__btn controler__btn--remove">remove</i>
         </div>
         </Aux>
 
